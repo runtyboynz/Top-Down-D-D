d792af83fede834b7f68d4c43f3530281c1dcc8c
R Pringle <reubenpringlenz@gmail.com>
2017-05-20 22:21:46 +1200

    Did CAMERA!!!

---
+++ Top Down Shooter Template/Library/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+	Vector3 velocity;
+	Rigidbody myRigidBody;
+
+	void Start () 
+	{
+		myRigidBody = GetComponent<Rigidbody> ();
+	
+	}
+
+	public void Move (Vector3 _velocity)
+	{
+		velocity = _velocity;
+	}
+
+	public void LookAt(Vector3 lookPoint)
+	{
+		Vector3 adjustPointHeight = new Vector3 (lookPoint.x, transform.position.y, lookPoint.z);//Adjusts ray point which is on the ground to player eye height.
+		transform.LookAt (adjustPointHeight);//makes the player look at the camera/mouse ray point.
+	}
+
+	public void FixedUpdate()
+	{
+		myRigidBody.MovePosition (myRigidBody.position + velocity * Time.fixedDeltaTime);
+	}
+}
