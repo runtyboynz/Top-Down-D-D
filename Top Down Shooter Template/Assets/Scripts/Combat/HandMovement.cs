8e8b3ec3f04ff83d6a4489c76551d01783e31c44
R Pringle <reubenpringlenz@gmail.com>
2017-06-13 13:49:49 +1200

    Reuben. Fixed bug of hand going returning to the wrong point when pc moves.

--- Top Down Shooter Template/Assets/Scripts/Combat/HandMovement.cs
+++ Top Down Shooter Template/Assets/Scripts/Combat/HandMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class HandMovement : MonoBehaviour 
 {
 
-	public Transform farPointAimerReach, combatAimer;
+	public Transform farPointAimerReachRH, farPointAimerReachLH, combatAimer;
 	public Transform handMovementRH, handMovementLH;
 
 
@@ -18,10 +18,11 @@ public class HandMovement : MonoBehaviour
 
 				if (Input.GetMouseButton(1))//If right mouse button is pushed down, shoot 
 		{
-			float x = -farPointAimerReach.transform.localPosition.x;
-			float y = -farPointAimerReach.transform.localPosition.z;
-			float z = 0;
+			float x = -farPointAimerReachRH.transform.localPosition.x / 6;//moves opposite of x
+			float y = -farPointAimerReachRH.transform.localPosition.z / 6;//z axis moves on the y axis opposingly
+			float z = 0;//default. The below equations make it move with the above axis.
 
+			//Sets restrictions for hand/arm movement distance.
 			x = Mathf.Clamp (x, -0.4f, 0.4f);
 			y = Mathf.Clamp (y, -0.35f, 0.25f);
 			z = Mathf.Clamp (z, -0.4f, 0f);
@@ -47,24 +48,51 @@ public class HandMovement : MonoBehaviour
 				z -= -y;
 			}
 */
-
-				
-			handMovementRH.transform.localPosition = new Vector3 (x, y, z);
-			//handMovementRH.localRotation = Quaternion.Euler (0,farPointAimerReach.localRotation.y,0);
+			handMovementRH.transform.localPosition = new Vector3 (x, y, z);//Sets up the new variable numbers that the hand movement will move along with mouse movement.
 		}
 				if (Input.GetMouseButton(0))//If left mouse button is pushed down, shoot 
 		{
-			handMovementLH.transform.localPosition = new Vector3 (farPointAimerReach.transform.localPosition.x, farPointAimerReach.transform.localPosition.y, farPointAimerReach.transform.localPosition.z);
+			float x = -farPointAimerReachLH.transform.localPosition.x / 6;//moves opposite of x
+			float y = -farPointAimerReachLH.transform.localPosition.z / 6;//z axis moves on the y axis opposingly
+			float z = 0;//default. The below equations make it move with the above axis.
+
+			//Sets restrictions for hand/arm movement distance.
+			x = Mathf.Clamp (x, -0.4f, 0.4f);
+			y = Mathf.Clamp (y, -0.35f, 0.25f);
+			z = Mathf.Clamp (z, -0.4f, 0f);
+
+			if (x > 0.1f) 
+			{
+				z -= x;
+			}
+
+			if (x < -0.1f) 
+			{
+				z -= -x;
+			}
+
+			if (y > 0.1f) 
+			{
+				z -= y;
+				//Debug.Log ("z: " + z);
+			}
+			/*
+			if (y < -0.1f) 
+			{
+				z -= -y;
+			}
+*/
+			handMovementLH.transform.localPosition = new Vector3 (x, y, z);//Sets up the new variable numbers that the hand movement will move along with mouse movement.
 		}
 
 		if (Input.GetMouseButtonUp(1))//If right mouse button is unclicked (For Hand Position to go back to default)
 		{
-			handMovementRH.transform.localPosition = transform.TransformPoint (Vector3.zero);//Snaps Origin point back to Default position
+			handMovementRH.transform.localPosition = new Vector3 (0,0,0);//Snaps Origin point back to Default position
 		}
 
-		if (Input.GetMouseButtonUp(0))//If right mouse button is unclicked (For Hand Position to go back to default)
+		if (Input.GetMouseButtonUp(0))//If left mouse button is unclicked (For Hand Position to go back to default)
 		{
-			handMovementLH.transform.localPosition = transform.TransformPoint (Vector3.zero);//Snaps Origin point back to Default position
+			handMovementLH.transform.localPosition = new Vector3 (0,0,0);//Snaps Origin point back to Default position
 		}
 	}
 }
--- Top Down Shooter Template/Assets/Scripts/PC, Camera/Player.cs
+++ Top Down Shooter Template/Assets/Scripts/PC, Camera/Player.cs
@@ -7,8 +7,13 @@ public class Player : MonoBehaviour
 {
 
 	public Transform motionPoint;//To get the character to move with the camera center point, aka camera.
-	public Transform farPointAimerOrigin, farPointAimerReach;//For Object to follow ray point.
+
+	public Transform farPointAimerOriginRH, farPointAimerOriginLH, farPointAimerReachRH, farPointAimerReachLH;//For Object to follow ray point.
+
+	public float farPointDefaultAimerReachY = 1.0f;//Sets default Aimer Reach Sphere Y axis.
+
 	public Transform closePointAim;//To register close attacks.
+	public Transform combatAimer;//The 1st/3rd Person Sphere that pc aims/reaches at/to when attacking.
 
 	public float moveSpeed = 5;
 
@@ -47,41 +52,66 @@ public class Player : MonoBehaviour
 			controller.LookAt(point);//Player looks at ray point.
 
 			//AIMING IN CLOSE COMBAT: Here we use two game objects to measure the x,y,z distance for how large/small your attack will be.
-			farPointAimerOrigin.LookAt (motionPoint);//looks at player/motion/center point
+			farPointAimerOriginRH.LookAt (motionPoint);//looks at player/motion/center point RH
+			farPointAimerOriginLH.LookAt (motionPoint);//looks at player/motion/center point LH
 
-			farPointAimerReach.position = ray.GetPoint (rayDistance);//Makes Reach Object follow mouse/camera ray.
-			farPointAimerReach.LookAt (motionPoint);//looks at player/motion/center point
+			farPointAimerReachRH.position = ray.GetPoint (rayDistance);//Makes Reach Object follow mouse/camera ray RH.
+			farPointAimerReachLH.position = ray.GetPoint (rayDistance);//Makes Reach Object follow mouse/camera ray LH.
+
+			farPointAimerReachRH.transform.position = new Vector3 (farPointAimerReachRH.transform.position.x, farPointAimerReachRH.transform.position.y + farPointDefaultAimerReachY, farPointAimerReachRH.transform.position.z);//Sets Mouse Aim Reach at higher Y, so he looks upward while freelooking RH.
+			farPointAimerReachLH.transform.position = new Vector3 (farPointAimerReachLH.transform.position.x, farPointAimerReachLH.transform.position.y + farPointDefaultAimerReachY, farPointAimerReachLH.transform.position.z);//Sets Mouse Aim Reach at higher Y, so he looks upward while freelooking LH.
+
+			farPointAimerReachRH.LookAt (motionPoint);//looks at player/motion/center point RH
+			farPointAimerReachLH.LookAt (motionPoint);//looks at player/motion/center point LH
 
 			//SECONDARY RAY: For Finishing up Combat mechanics + Enabling 1st Person.
-			closePointAim.LookAt (farPointAimerReach.position);
+			closePointAim.LookAt (farPointAimerReachRH);//Makes Hands/attacks aim at the combat aimer which it will reach/aim at/to.
 		}
+
 
 		//Weapon Right Hand Input
 
+		if (Input.GetMouseButton(1))//If right mouse button is pushed down, shoot 
+		{
+			farPointAimerReachRH.transform.position = new Vector3 (farPointAimerReachRH.transform.position.x, farPointAimerReachRH.transform.position.y - farPointDefaultAimerReachY, farPointAimerReachRH.transform.position.z);//Resets Mouse Aim Sphere to default Y position RH.
+			closePointAim.LookAt (farPointAimerReachRH.position);//Makes Hands/attacks aim at the combat aimer which it will reach/aim at/to.
+			weaponController.ShootRH ();
+		}
+
 		if (Input.GetMouseButtonDown(1))//If right mouse button is clicked (For Aiming Origin and Reach Gameobjects to register)
 		{
-			farPointAimerOrigin.transform.position = new Vector3 (farPointAimerReach.transform.position.x, farPointAimerReach.transform.position.y, farPointAimerReach.transform.position.z);
-			farPointAimerOrigin.localRotation = Quaternion.Euler (0,farPointAimerOrigin.localRotation.y,0);//stops object from tilting when closer to player.
-			farPointAimerReach.localRotation = Quaternion.Euler (0,farPointAimerReach.localRotation.y,0);//stops object from tilting when closer to player.
+			farPointAimerOriginRH.transform.position = new Vector3 (farPointAimerReachRH.transform.position.x, farPointAimerReachRH.transform.position.y, farPointAimerReachRH.transform.position.z);//Sets Origin Aimer at the exact point Reach aim is when mouse is pushed down.
+			farPointAimerOriginRH.localRotation = Quaternion.Euler (0,farPointAimerOriginRH.localRotation.y,0);//stops object from tilting when closer to player.
+			farPointAimerReachRH.localRotation = Quaternion.Euler (0,farPointAimerReachRH.localRotation.y,0);//stops object from tilting when closer to player.
 			//^^Sends Origin Gameobject to Reach Object position to measure the distance so Reach Object can measure how far away it moves from the origin point^^.
 		}
 
 		if (Input.GetMouseButtonUp(1))//If right mouse button is unclicked (For Aiming Origin to go back to default)
 		{
-			farPointAimerOrigin.transform.position = transform.TransformPoint (Vector3.zero);//Snaps Origin point back to Default position
+			farPointAimerOriginRH.transform.position = transform.TransformPoint (Vector3.zero);//Snaps Origin point back to Default position
 		}
 
-		if (Input.GetMouseButton(1))//If right mouse button is pushed down, shoot 
-		{
-			weaponController.ShootRH ();
-		}
 
 		//Weapon Left Hand Input
 
 		if (Input.GetMouseButton(0))//If left mouse button is pushed down, shoot 
 		{
+			farPointAimerReachLH.transform.position = new Vector3 (farPointAimerReachLH.transform.position.x, farPointAimerReachLH.transform.position.y - farPointDefaultAimerReachY, farPointAimerReachLH.transform.position.z);//Resets Mouse Aim Sphere to default Y position LH.
+			closePointAim.LookAt (farPointAimerReachLH.position);//Makes Hands/attacks aim at the combat aimer which it will reach/aim at/to.
 			weaponController.ShootLH ();
 		}
 
+		if (Input.GetMouseButtonDown(0))//If left mouse button is clicked (For Aiming Origin and Reach Gameobjects to register)
+		{
+			farPointAimerOriginLH.transform.position = new Vector3 (farPointAimerReachLH.transform.position.x, farPointAimerReachLH.transform.position.y, farPointAimerReachLH.transform.position.z);//Sets Origin Aimer at the exact point Reach aim is when mouse is pushed down.
+			farPointAimerOriginLH.localRotation = Quaternion.Euler (0,farPointAimerOriginLH.localRotation.y,0);//stops object from tilting when closer to player.
+			farPointAimerReachLH.localRotation = Quaternion.Euler (0,farPointAimerReachLH.localRotation.y,0);//stops object from tilting when closer to player.
+			//^^Sends Origin Gameobject to Reach Object position to measure the distance so Reach Object can measure how far away it moves from the origin point^^.
+		}
+
+		if (Input.GetMouseButtonUp(0))//If left mouse button is unclicked (For Aiming Origin to go back to default)
+		{
+			farPointAimerOriginLH.transform.position = transform.TransformPoint (Vector3.zero);//Snaps Origin point back to Default position
+		}
 	}
 }
